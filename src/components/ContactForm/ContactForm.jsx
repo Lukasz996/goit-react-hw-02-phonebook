c7cdda7c443d406b1c28311db260fd3e59fd9f7e
Łukasz <lukaszkogut1996@gmail.com>
2023-05-21 18:56:23 +0200

    add ContactForm

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,12 @@
+// import PropTypes from 'prop-types';
+import { Component } from 'react';
+// import css from './ContactForm.module.css';
+
+class ContactForm extends Component {
+
+
+}
+
+
+
+export default ContactForm;
